aeef5c76411765e917c3e6872d50244a1e50e15c
3599josephw <3599@holbertonschool.com>
2021-09-29 15:23:14 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - stuff
+ * @n: the int being checked
+ * Return: yeehaw
+ */
+int print_last_digit(int n)
+{
+n = n % 10;
+_putchar(n + '0');
+return (n);
+}
