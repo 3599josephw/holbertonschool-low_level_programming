73167911918b487e94f30352822c5082350f8b6d
3599josephw <3599@holbertonschool.com>
2021-09-29 12:21:15 -0700

    Correct 3 file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - inception babey
+ * @c: the char being checked
+ * Return: On success 1.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+else
+	{
+	return (0);
+	}
+}
