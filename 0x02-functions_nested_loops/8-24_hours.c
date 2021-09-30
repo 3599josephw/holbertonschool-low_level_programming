bfc4c145a998941fe2615898e0d35db273fe4e3b
3599josephw <3599@holbertonschool.com>
2021-09-30 07:03:15 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,22 +10,44 @@ int a;
 int b;
 int c;
 int d;
-for (a = '0'; a < '3'; a++)
+for (a = '0'; a <= '2'; a++)
 {
-for (b = '0'; b < '4'; b++)
-{
-	for (c = '0'; c < '6'; c++)
+	if (a < '2')
 	{
-		for (d = '0'; d <= '9'; d++)
+		for (b = '0'; b <= '9'; b++)
 		{
-		_putchar(a);
-		_putchar(b);
-		_putchar(':');
-		_putchar(c);
-		_putchar(d);
-		_putchar('\n');
+			for (c = '0'; c <= '5'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+				_putchar(a);
+				_putchar(b);
+				_putchar(':');
+				_putchar(c);
+				_putchar(d);
+				_putchar('\n');
+				}
+			}
+		}
 	}
+	else
+	{
+		for (b = '0'; b <= '3'; b++)
+		{
+			for (c = '0'; c <= '5'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+				_putchar(a);
+				_putchar(b);
+				_putchar(':');
+				_putchar(c);
+				_putchar(d);
+				_putchar('\n');
+				}
+			}
+		}
 	}
-	}
-	}
+}
+
 }
