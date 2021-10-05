f03da4dfeff0f2c7a591c9e0986da6b5cf8c8b03
3599josephw <3599@holbertonschool.com>
2021-10-05 11:54:09 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,28 @@
 
 void print_rev(char *s)
 {
+if (s[0] == '\0')
+{
+	_putchar('\n');
+}
+else
+{
 	while (*s != '\0')
 	{
 		s++;
 	}
+
+	s--;
+
 	while (*s >= 0)
 	{
 		_putchar(*s);
 		s--;
+		if (*s == 0)
+		{
+			break;
+		}
 	}
 	_putchar('\n');
+}
 }
