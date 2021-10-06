84fbcfed2babab39406c6d686c9cce9811d7f42a
3599josephw <3599@holbertonschool.com>
2021-10-06 08:53:41 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * puts_half - function
+ * @str: character
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, halfway, len;
+
+
+	i = 0;
+	len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	if (len % 2 == 0)
+	{
+		halfway = len / 2;
+	}
+	else
+	{
+		halfway = (len - 1) / 2;
+	}
+
+	for (i = len; i >= 0; i--)
+	{
+		str--;
+	}
+	for (i = 0; i <= len; i++)
+	{
+
+		if (i > halfway)
+		{
+			_putchar(*str);
+		}
+		str++;
+	}
+	_putchar('\n');
+}
