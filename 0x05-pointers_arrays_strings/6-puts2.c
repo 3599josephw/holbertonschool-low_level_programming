af587cd6d229c849ee7ca0ac30dac28613f40c1e
3599josephw <3599@holbertonschool.com>
2021-10-06 06:45:45 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,12 @@
 
 void puts2(char *str)
 {
-
 	while (*str != '\0')
-		{
-			_putchar(*str);
-			str+=2;
-		}
-        _putchar('\n');
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
 
 
 
