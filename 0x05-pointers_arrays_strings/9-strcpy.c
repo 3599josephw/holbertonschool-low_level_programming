0c853996776004f78409c5541305d388eb2934de
3599josephw <3599@holbertonschool.com>
2021-10-06 10:19:25 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcopy - copy a string
+ * *_strcopy - copy a string
  * @dest: variable string is copied to
  * @src: string being copied
  * Return: pointer to dest
@@ -14,8 +14,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	i++;
-	dest[i] = '\0';
+	dest[i++] = '\0';
 
 	return (dest);
 
