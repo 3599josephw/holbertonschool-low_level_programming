0133c28af0d0e1c9d3826defa33c7c081d2dadbe
3599josephw <3599@holbertonschool.com>
2021-10-11 17:01:26 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strpbrk - function
+ * @s: variable
+ * @accept: variable
+ * Return: return
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+    int a, b;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while (accept[b] != '\0')
+		{
+			if (s[a] == accept[b])
+			{
+				return (s + a);
+			}
+			b++;
+		}
+		a++;
+	}
+	return ('\0');
+
+}
