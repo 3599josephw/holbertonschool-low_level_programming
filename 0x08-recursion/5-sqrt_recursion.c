9e4fa28fe4c32a5d6595c807da49fc4d953c52f2
3599josephw <3599@holbertonschool.com>
2021-10-12 15:11:44 -0700

    betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,92 @@
+int _sqrt_num(int low, int high, int i);
+int prime_num(int n, int i);
+
 #include "main.h"
 /**
  * _sqrt_recursion - find square root of number
- * @x: number entered
+ * @n: number entered
  * Return: square root
  */
 
 int _sqrt_recursion(int n)
 {
-    int start = 0, end = n, mid, ans;
+	int ans, ifPrime;
 
-    while (start <= end)
-    {
-	mid = (start + end) / 2;
+	if (n < 0)
+	{
+		return (-1);
+	}
 
-	if (mid * mid == n)
+	ifPrime = prime_num(n, 2);
+
+	if (ifPrime == 1)
 	{
-	    ans = mid;
-	    break;
+		return (-1);
 	}
-	if (mid * mid < n)
+
+	ans = _sqrt_num(0, n, n);
+
+	return (ans);
+}
+
+/**
+ * _sqrt_num - find the square root
+ * @low: low range
+ * @high: high range
+ * @i: iterator
+ * Return: the square root
+ */
+int _sqrt_num(int low, int high, int i)
+{
+	int mid;
+
+	if (low <= high)
 	{
-	    ans=start;
-	    start = mid + 1;
+		mid = (low + high) / 2;
+
+		if ((mid * mid <= i)
+		&& ((mid + 1) * (mid + 1) > i))
+		{
+			return (mid);
+		}
+
+		else if (mid * mid < i)
+		{
+			return (_sqrt_num(mid + 1, high, i));
+		}
+		else
+		{
+			return (_sqrt_num(low, mid - 1, i));
+		}
 	}
-	else
+	return (low);
+}
+
+/**
+ * prime_num - determine if prime
+ * @n: number
+ * @i: iterator
+ * Return: 1 if prime, 0 if not prime
+ */
+
+int prime_num(int n, int i)
+{
+
+	if (n == 2)
 	{
-	    end = mid - 1;
+		return (1);
 	}
-    }
-    return ans;
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
+	return (prime_num(n, i + 1));
 }
