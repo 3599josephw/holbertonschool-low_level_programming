bafb0aca7f1c6e41cff1a027e2d941a5cbb552d3
3599josephw <3599@holbertonschool.com>
2021-10-11 18:13:05 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - function
+ * @a: variable
+ * @size: of array
+ * Return: return
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, suma = 0, sumb = 0;
+
+
+	for (i = 0; i < size; i++)
+	{
+		suma = suma + a[i][i];
+	}
+	printf("%d, ", suma);
+
+	for (i = size; i > 0; i--)
+	{
+		sumb = sumb + a[i][i];
+	}
+	printf("%d\n", sumb);
+
+}
