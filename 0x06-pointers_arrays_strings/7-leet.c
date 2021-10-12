1a923afe40d44a853389e8f8d5f89c890471d825
3599josephw <3599@holbertonschool.com>
2021-10-12 13:30:47 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *leet - leetspeak
+ * @ch: the string given
+ * Return: leetified string
+ */
+
+char *leet(char *ch)
+{
+int a, b;
+char leet[11] = {"aAeEoOtTlL"};
+char l33t[11] = {"4433007711"};
+
+
+	a = 0;
+	b = 0;
+
+	while (ch[a] != '\0')
+	{
+		while (leet[b] != '\0' && ch[a] != leet[b])
+		{
+			b++;
+		}
+		if (b < 10)
+		{
+		ch[a] = l33t[b];
+		}
+		a++;
+	}
+	return (ch);
+}
