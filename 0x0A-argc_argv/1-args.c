2c3136a783ef845010ac3b64a85bd50472088e02
3599josephw <3599@holbertonschool.com>
2021-10-15 10:07:49 -0700

    Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints name of file
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
