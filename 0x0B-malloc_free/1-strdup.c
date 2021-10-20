b415886c0a7cdc6a7142eba49d1ebacdfe1d13e2
3599josephw <3599@holbertonschool.com>
2021-10-20 09:09:04 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_strdup - duplicate string
+ * @str: string given
+ * Return: pointer to duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int i, size;
+	char *array;
+
+	size = strlen(str);
+
+	array = malloc((size + 1) * sizeof(char));
+
+	if (array == NULL || size == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = str[i];
+	}
+
+	return (array);
+
+
+
+
+}
