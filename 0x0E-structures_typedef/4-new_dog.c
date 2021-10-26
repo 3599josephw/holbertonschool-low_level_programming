ff5975378b5e6876e9bdb684d78c2da47da67022
3599josephw <3599@holbertonschool.com>
2021-10-26 11:30:59 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,18 +6,18 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * Return: void
+ * Return: newdog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	char name2 = *name, owner2 = *owner;
 
 	newdog = malloc(sizeof(dog_t));
 
-
-	newdog->name = name;
+	newdog->name = &name2;
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = &owner2;
 
 	if (newdog == NULL)
 		return (NULL);
