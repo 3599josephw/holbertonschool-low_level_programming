a836af033cc8e4f61bdd52b31e4200155bfdd6fb
3599josephw <3599@holbertonschool.com>
2021-10-26 11:33:57 -0700

    moved if statement

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,17 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	char name2 = *name, owner2 = *owner;
 
 	newdog = malloc(sizeof(dog_t));
 
-	newdog->name = &name2;
-	newdog->age = age;
-	newdog->owner = &owner2;
-
 	if (newdog == NULL)
 		return (NULL);
+
+
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
+
 
 	return (newdog);
 }
