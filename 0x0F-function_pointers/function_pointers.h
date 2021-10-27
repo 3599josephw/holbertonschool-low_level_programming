576cac69d91a21ab190860e6597c53280a2f113f
3599josephw <3599@holbertonschool.com>
2021-10-27 10:14:31 -0700

    fixed include guard

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
-#include <stdlib.h>
-#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
