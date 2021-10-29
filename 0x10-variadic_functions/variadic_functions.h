82b80093fb9359a56101bc266d4015a99f90d741
3599josephw <3599@holbertonschool.com>
2021-10-29 10:31:48 -0700

    header fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,6 @@
 #define VARIADIC_FUNCTIONS
 #include <stdarg.h>
 
-typedef struct format {
-	char *f
-
-}
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
