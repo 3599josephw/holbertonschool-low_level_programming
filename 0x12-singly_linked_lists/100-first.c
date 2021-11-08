143e586d537a3b7a3313999f2ac5057a7a18dad9
3599josephw <3599@holbertonschool.com>
2021-11-08 11:57:31 -0800

    advanced task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * hare - prints out a line before main is called
+ * Return: nothing
+ */
+
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
