6cc921058fac59137164f1ced625096afb88234e
3599josephw <3599@holbertonschool.com>
2021-11-17 10:49:14 -0800

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(letters);
 
 	if (buf == NULL)
-		return ('\0');
+		return (0);
 
 	rd = read(fd, buf, letters);
 
@@ -30,10 +30,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (wr == -1)
 		return (0);
-	free(buf);
 
 	close(fd);
-
+	free(buf);
 
 	return (wr);
 }
