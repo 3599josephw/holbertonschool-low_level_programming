536e090a76a21011c5e76a065df5324fe4e03aed
3599josephw <3599@holbertonschool.com>
2021-11-17 10:25:50 -0800

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and pritns to standard output
+ * @filename: the file to be read
+ * @letters: how many letters should be read
+ * Return: actual number o fletters to read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t *buf;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if(fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+
+	if (buf == NULL)
+		return ('\0');
+
+	read(fd, buf, letters);
+
+	write(1, buf, letters);
+	close(fd);
+
+
+	return (letters);
+}
