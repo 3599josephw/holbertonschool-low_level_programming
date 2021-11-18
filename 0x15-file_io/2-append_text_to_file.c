63d918b374483dd8e9d245d3000de4af7f65e37c
3599josephw <3599@holbertonschool.com>
2021-11-18 08:35:24 -0800

    length check

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,10 +7,12 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, j;
+	int fd, j, len;
 
 	if (filename == NULL)
 		return (-1);
+
+	len = strlen(text_content);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
@@ -21,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	j = write(fd, text_content, strlen(text_content));
+	j = write(fd, text_content, len);
 
 	if (j == -1)
 		return (-1);
