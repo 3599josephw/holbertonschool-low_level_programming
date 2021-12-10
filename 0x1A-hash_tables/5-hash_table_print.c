0bfd31e6d1ae831e5dad9f7ea36336125df9b539
3599josephw <3599@holbertonschool.com>
2021-12-10 14:55:08 -0800

    less than or equal to changed to less than

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -16,7 +16,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 				j = i + 1;
-				while (j <= ht->size)
+				while (j < ht->size)
 				{
 					flag = 0;
 					if (ht->array[j] == NULL)
