5e7a42aad941b63967ef10ef6b0b97b1d4aeec2f
3599josephw <3599@holbertonschool.com>
2021-12-12 08:41:58 -0800

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,16 +13,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || (strcmp("", key) == 0))
 		return (0);
-
 	new = malloc(sizeof(hash_node_t));
-
 	if (new == NULL)
 	{
 		free(new);
 		return (0);
 	}
 	index = key_index((const unsigned char *)key, ht->size);
-
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
