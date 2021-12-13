ab89c71ce589e5b6637f5b2666c1050f9ced73f6
3599josephw <3599@holbertonschool.com>
2021-12-13 08:11:54 -0800

    null checks

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	if (ht == NULL)
-		return (NULL)
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
