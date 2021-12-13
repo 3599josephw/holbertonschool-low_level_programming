1bc5b482b5ae4939f2d59c41a0aca2ce0aac6af1
3599josephw <3599@holbertonschool.com>
2021-12-13 07:40:53 -0800

    null check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 
 	new_table = malloc(sizeof(hash_table_t));
-	new_table->size = size;
+	if (new_table == NULL)
+		return (NULL);
+
 	new_table->array = malloc(sizeof(hash_node_t *) * (size));
 
-	if (new_table == NULL || new_table->array == NULL)
+	if (new_table->array == NULL)
 		return (NULL);
+
+	new_table->size = size;
 
 	while (i < size)
 	{
