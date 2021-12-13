3ac1df7df9db25a152074a4511e83a10ff1c83f5
3599josephw <3599@holbertonschool.com>
2021-12-13 07:38:18 -0800

    removed size check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i = 0;
 
+
 	new_table = malloc(sizeof(hash_table_t));
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * (size));
