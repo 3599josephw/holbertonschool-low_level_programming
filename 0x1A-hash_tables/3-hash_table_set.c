ff94e0ccabe9dbc016a437ce845471c1f34baf75
3599josephw <3599@holbertonschool.com>
2021-12-13 07:52:11 -0800

    ht check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new, *current;
 
-	if (key == NULL || (strcmp("", key) == 0))
+	if (key == NULL || (strcmp("", key) == 0) || ht == NULL)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
