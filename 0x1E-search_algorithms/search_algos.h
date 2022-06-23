7ff35d6d848b271f2d8551d406bfd6f11f0af6da
3599josephw <3599@holbertonschool.com>
2022-06-23 18:18:32 -0500

    advanced binary algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,9 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+int binary_search1(int *array, size_t size, int value);
+
+int binary_search(int *array, int l1, int r1, int val);
+int advanced_binary(int *array, size_t size, int value);
+
 #endif
