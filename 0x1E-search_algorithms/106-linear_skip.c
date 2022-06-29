f9721cb065bedca360c228b43d832bdc5be2d140
3599josephw <3599@holbertonschool.com>
2022-06-29 11:14:08 -0500

    multiple occurence fix

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -21,10 +21,6 @@ skiplist_t *linear_skip(skiplist_t *list, int value) {
         prev = current;
         current = current->express;
         printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-        if (current->n == value)
-        {
-            return (current);
-        }
         if (current->n > value)
         {
             printf("Value found between indexes [%lu] and [%lu]\n", prev->index, current->index);
